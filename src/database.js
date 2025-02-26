c551cc84791e44cfe0f40cb546340a503f829e8e
Alex <alex.holder@bigfootds.com>
2025-02-26 20:45:13 +1100

    feat: databasehealth route

--- src/database.js
+++ src/database.js
@@ -1,4 +1,5 @@
-const { default: mongoose } = require("mongoose")
+const { default: mongoose } = require("mongoose");
+const { User } = require("./models/UserModel");
 
 
 async function databaseConnect(targetDatabaseURL = null){
